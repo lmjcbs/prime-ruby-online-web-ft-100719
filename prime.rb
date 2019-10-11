675d8cea9f02a8ed0cbb2f41ee56299957deaf04
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 14:12:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 
 def prime?(value)
-  return false if value < 2
+  #return false if value < 2
   (2...value).each { |n| return false if value % n == 0 }
   value < 2 ? false : true
 end
