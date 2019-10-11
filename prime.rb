32b34d97449be56a9ad63c9e9ef6733833d5ba29
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 14:00:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require 'pry'
+
+def prime?(prime)
+  return false if prime != prime.abs || prime < 2
+  (2...prime).each do |n|
+    if prime % n == 0
+      return false
+    end
+  end
+  true
+end
